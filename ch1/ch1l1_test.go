89156ba89c4e2103b536269d1ba2cbc9af52e1fb
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-09-09 08:02:57 +0900

    1: add ch1l1.go and the test - practice ch1l1

---
+++ ch1/ch1l1_test.go
@@ -0,0 +1,72 @@
+package ch1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDebugEncryptDecrypt(t *testing.T) {
+	type testCase struct {
+		masterKey string
+		iv        string
+		password  string
+		expectedE string
+		expectedD string
+	}
+
+	const masterKey = "kjhgfdsaqwertyuioplkjhgfdsaqwert"
+	const iv = "1234567812345678"
+
+	runCases := []testCase{
+		{masterKey, iv, "k33pThisPasswordSafe", encrypt("k33pThisPasswordSafe", masterKey, iv), "k33pThisPasswordSafe"},
+		{masterKey, iv, "12345", encrypt("12345", masterKey, iv), "12345"},
+		{masterKey, iv, "thePasswordOnMyLuggage", encrypt("thePasswordOnMyLuggage", masterKey, iv), "thePasswordOnMyLuggage"},
+		{masterKey, iv, "pizza_the_HUt", encrypt("pizza_the_HUt", masterKey, iv), "pizza_the_HUt"},
+	}
+
+	submitCases := append(runCases, []testCase{
+		{masterKey, iv, "aNewPassword", encrypt("aNewPassword", masterKey, iv), "aNewPassword"},
+		{masterKey, iv, "edgeCaseTest", encrypt("edgeCaseTest", masterKey, iv), "edgeCaseTest"},
+	}...)
+
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
+	skipped := len(submitCases) - len(testCases)
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range testCases {
+		encrypted, decrypted := debugEncryptDecrypt(test.masterKey, test.iv, test.password)
+		if encrypted != test.expectedE || decrypted != test.expectedD {
+			failCount++
+			t.Errorf(`---------------------------------
+Inputs:      masterKey: %v, iv: %v, password: %v
+Expecting:   Encrypted: %v, Decrypted: %v
+Actual:      Encrypted: %v, Decrypted: %v
+Fail
+`, test.masterKey, test.iv, test.password, test.expectedE, test.expectedD, encrypted, decrypted)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Inputs:      masterKey: %v, iv: %v, password: %v
+Expecting:   Encrypted: %v, Decrypted: %v
+Actual:      Encrypted: %v, Decrypted: %v
+Pass
+`, test.masterKey, test.iv, test.password, test.expectedE, test.expectedD, encrypted, decrypted)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
+var withSubmit = true
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paokimsiwoong/cryptography_practice
+
+go 1.24.0
